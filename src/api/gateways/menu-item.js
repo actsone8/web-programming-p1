0b93b6a646de788eb3b71a748e9354bd0d72d9fe
Scott Laska <actsone8@gmail.com>
2024-02-19 10:20:37 -0600

    added some sample menu items

--- src/api/gateways/menu-item.js
+++ src/api/gateways/menu-item.js
@@ -1,3 +1,7 @@
-// exports.findAll = () => {
-//   return new Promise(resolve => resolve(menuItems));
-// };
+const menuItems = ['sofa', 123, true, [1, 2, 3], { height: 10 }];
+console.log("hi mom")
+console.log(menuItems);
+
+exports.findAll = () => {
+  return new Promise(resolve => resolve(menuItems));
+};
